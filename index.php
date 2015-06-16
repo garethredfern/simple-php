4f65b00f36db8964afeea50baff04696d0b5a310
Gareth Redfern <gareth.redfern@gmail.com>
2015-06-16 07:09:42 +0100

    added example files

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+
+$log->addWarning('Foo');
+
+echo "Hello World!";
