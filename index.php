a3eaeed5878ac7c635641720034058d6e456560e
Gareth Redfern <gareth.redfern@gmail.com>
2015-06-17 07:09:26 +0100

    added namespacing example

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 require 'vendor/autoload.php';
 
-$log = new Monolog\Logger('name');
-$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('app.log', Monolog\Logger::WARNING));
 
 $log->addWarning('Foo');
 
